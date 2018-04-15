466ec4c06ae40a16bbc73dd8ea7478528eeb9ac3
zakaria_orion <zakaria.orion@github.com>
2018-04-15 17:19:08 +0100

    Initial commit

---
+++ src/Workshop.java
@@ -0,0 +1,17 @@
+
+public class Workshop {
+	
+	
+	
+	public static void main(String[] args) { 
+	  Personne unepersonne = new Personne();
+	  unepersonne.setNom("Alucard");
+	  unepersonne.setPrenom("dracula");
+	  System.out.println("Bonjour, je m'appelle " + unepersonne.getNom() + " " +
+			  unepersonne.getPrenom()); 
+		
+		
+	}   
+
+	}
+
